d99359731cbe9108ce257b1290a697ddf1757054
Gavriil Tzigieras <psygt2@exmail.nottingham.ac.uk>
2020-11-28 15:12:11 +0000

    main

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    public static Stage primaryStage;
+    public static GridPane splitGrid;
+    private static Scene splitScene;
+
+    public static void main(String[] args) {
+        launch(args);
+        System.out.println("Done");
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        Main.primaryStage = primaryStage;
+
+    }
 }
